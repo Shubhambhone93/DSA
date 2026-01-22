cad077317d7e3d24f1cbfd00b62a674e45a5d9c5
Shubhambhone93 <bhoneshubham02@gmail.com>
2026-01-22 15:39:09 +0530

    pattern

---
+++ lecture_4/lecture_5/pattern_12.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cin>>n;
+   int m=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" \t ";
+        }
+        for( int k=1;k<=2*i-1;k++){
+            cout<<m<<" \t";
+            m++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
