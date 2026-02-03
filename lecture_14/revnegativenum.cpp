f739c700f56c9e03158eae41f46ac788a1ca3433
Shubhambhone93 <bhoneshubham02@gmail.com>
2026-02-03 16:07:45 +0530

    negativ num

--- lecture_14/revnegativenum.cpp
+++ lecture_14/revnegativenum.cpp
@@ -3,10 +3,18 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int rem=0;
-    while(n){
-        rem=10*rem+n%10;
+    int rev=0;
+    while(n!=0){
+        int k=n%10;
+        rev=rev*10+k;
         n=n/10;
     }
-    cout<<rem;
+    cout<<rev<<endl;
+    //for loop
+    for(int i=n;i!=0;i=i/10){
+        int k=i%10;
+        rev=rev*10+k;
+    }
+    cout<<rev<<endl;
+    return 0;
 }
