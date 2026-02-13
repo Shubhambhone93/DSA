93f9c1ee1ca3e6f322a2d1c079528ed219e46325
Shubhambhone93 <bhoneshubham02@gmail.com>
2026-02-13 20:37:23 +0530

    boston number

--- lecture_14/boston_num.cpp
+++ lecture_14/boston_num.cpp
@@ -1,44 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-	int digitsum(int n);
-		int sum=0;
-		while(n!=0){
-			int k=n%10;
-			sum+=k;
-			n/=10;
-		}
-		return sum;
 
-	bool prime(int n){
-		for(int i=2;i*i<=n;i++){
-			if(n%i==0){
-				return false;
-			}
-		}
-		return true;
-	}
-	int main(){
-		int n;
-		cin>>n;
-		int sum=0;
-		int factor=0;
-		sum=digitsum(n);
-		for(int i=2;i<=n;i++){
-			while(n%i==0){
-				if(prime(i)){
-					factor+=digitsum(i);
-				}
-				n=n/i;
-			}
-		}
-		cout<<sum<<endl;
-		cout<<factor<<endl;
-		if(sum==factor){
-			cout<<"boston"<<endl;
-		}
-		else{
-			cout<<"Not Boston"<<endl;
-		}
-        return 0;
+// Function to calculate sum of digits
+int digitSum(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int original = n;
+    int sumDigits = digitSum(n);
+    int sumPrimeFactors = 0;
+
+    // Check for prime factors
+    for (int i = 2; i <= n; i++) {
+        while (n % i == 0) {
+            sumPrimeFactors += digitSum(i);
+            n /= i;
+        }
     }
 
+    // Check if number is composite and Boston
+    if (sumDigits == sumPrimeFactors && original != 1) {
+        cout << original << " is a Boston Number.";
+    } else {
+        cout << original << " is not a Boston Number.";
+    }
+
+    return 0;
+}
