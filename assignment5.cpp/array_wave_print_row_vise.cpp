b2f0a71367cd55a1f37341c4d7ca20c394dcb838
Shubhambhone93 <bhoneshubham02@gmail.com>
2026-03-18 11:44:52 +0530

    array wave print row wise

---
+++ assignment5.cpp/array_wave_print_row_vise.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    int arr[10][10];
+
+    // Input
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Wave Print Row-wise
+    for(int i = 0; i < m; i++) {
+        if(i % 2 == 0) {
+            // left to right
+            for(int j = 0; j < n; j++) {
+                cout << arr[i][j] << ", ";
+            }
+        } else {
+            // right to left
+            for(int j = n - 1; j >= 0; j--) {
+                cout << arr[i][j] << ", ";
+            }
+        }
+    }
+
+    cout << "END";
+
+    return 0;
+}
