f12233eb08064bb4867a72ac3257d096d3308c8d
Shubhambhone93 <bhoneshubham02@gmail.com>
2026-06-17 11:18:06 +0530

    Q_789 leetcode

---
+++ assignment5.cpp/k_revrese.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+
+    }
+    int k;
+    cin>>k;
+    int a=0;
+    for(int i=0;i<n/k;i++){
+        reverse(nums.begin()+a,nums.begin()+a+k);
+        a=a+k;
+    }
+    reverse
+}
